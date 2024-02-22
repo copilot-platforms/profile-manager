1d5e0936154ed85edb67b0ba3e07f3412976aacf
Rojan Rajbhandari <rrojan20@gmail.com>
2024-02-22 18:19:17 +0545

    fix: minor fixes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export const copilotAPIUrl = process.env.COPILOT_API_URL || '';
 export const copilotAPIKey = process.env.COPILOT_API_KEY || '';
 export const apiUrl = `${process.env.VERCEL_ENV === 'development' ? 'http://' : 'https://'}${process.env.VERCEL_URL}`;
+export const copilotPrimaryColor = process.env.COPILOT_PRIMARY_COLOR || '';
