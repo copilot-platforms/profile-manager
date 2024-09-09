21b0d5c516f20521383f6dc55dc6d495a9f688de
Sazan Rajbhandari <sazanrjb@gmail.com>
2024-09-09 16:22:09 +0545

    Implement Sentry sourcemaps (#29)
    * chore: include docker-compose file for local
    
    * feat: upload sourcemaps to Sentry
    
    * chore: add back previous key in next config
    
    * fix: remove sentry dns
    
    * chore: add reply block

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,14 +3,18 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { SentryConfig } from '@/config';
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  dsn: SentryConfig.DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+if (dsn) {
+  Sentry.init({
+    dsn,
+
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,17 +2,18 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { SentryConfig } from '@/config';
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  dsn: SentryConfig.DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+if (dsn) {
+  Sentry.init({
+    dsn,
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-});
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
