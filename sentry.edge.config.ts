62237873cae875ef649cc43d97379627f9bb442c
Sajan Rajbhandari <sazanrjb@gmail.com>
2024-09-09 15:14:07 +0545

    feat: upload sourcemaps to Sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,12 +3,12 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { SentryConfig } from '@/config';
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: SentryConfig.DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
+  dsn: "https://59afc8bed585f3f0b3c89dc416ef316c@o4504254375133184.ingest.us.sentry.io/4506911559450624",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
